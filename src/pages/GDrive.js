f32f8ab99cfb68bfe8a0fd38385ed184559511f4
N8Sayer <forcelord50@gmail.com>
2017-11-10 07:21:07 -0600

    tweaks to gdrive link

--- src/pages/GDrive.js
+++ src/pages/GDrive.js
@@ -4,7 +4,7 @@ const GDrive = props => {
   return(
     <div>
       <p>These are some of the past projects I've completed within Google's G Suite using Google Apps Script to extend the functionality of simple spreadsheets.</p>
-      <iframe src='https://drive.google.com/drive/folders/0BxJeGYCNyrP5MTBpdng3cS1CUVk' height="500" width="700" frameBorder="0" />
+      <iframe src='https://drive.google.com/embeddedfolderview?id=0BxJeGYCNyrP5MTBpdng3cS1CUVk#list' height="500" width="700" frameBorder="0" />
     </div>
   );
 };
