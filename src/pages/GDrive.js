892c0239b420a9edb19172a3799e49f6ac7024e3
N8Sayer <forcelord50@gmail.com>
2017-11-10 07:27:39 -0600

    Last update for today
    1.1

--- src/pages/GDrive.js
+++ src/pages/GDrive.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const GDrive = props => {
   return(
     <div>
-      <p>These are some of the past projects I've completed within Google's G Suite using Google Apps Script to extend the functionality of simple spreadsheets.</p>
+      <p>These are some of the past projects I've completed using Google Apps Script to extend the functionality of simple spreadsheets.</p>
       <iframe src='https://drive.google.com/embeddedfolderview?id=0BxJeGYCNyrP5MTBpdng3cS1CUVk#list' height="500" width="700" frameBorder="0" />
     </div>
   );
