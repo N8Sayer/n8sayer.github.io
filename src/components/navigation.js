953d05b81a73105df94f74fb2829105ef1fbbb8c
N8Sayer <forcelord50@gmail.com>
2018-04-21 10:16:14 -0500

    removed images

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-  return (
-    <ul className='navbar list-group'>
-      <Link to='/'>
-        <li className='list-group-item'>Home</li>
-      </Link>
-      <Link to='/resume'>
-        <li className='list-group-item'>Resume</li>
-      </Link>
-      <Link to='/projects'>
-        <li className='list-group-item'>Web Projects</li>
-      </Link>
-      <Link to='/gdrive'>
-        <li className='list-group-item'>Apps Script Projects</li>
-      </Link>
-      <a href='https://www.upwork.com/freelancers/~0184df6f716ff64f05'>
-        <li className='list-group-item'><img src='src/images/upwork-icon.png' /></li>
-      </a>
-      <a href='https://github.com/N8Sayer'>
-        <li className='list-group-item'><img src='src/images/github-icon.png' height='25px' /></li>
-      </a>
-    </ul>
-  );
+	return (
+		<ul className="navbar list-group">
+			<Link to="/">
+				<li className="list-group-item">Home</li>
+			</Link>
+			<Link to="/resume">
+				<li className="list-group-item">Resume</li>
+			</Link>
+			<Link to="/projects">
+				<li className="list-group-item">Web Projects</li>
+			</Link>
+			<Link to="/gdrive">
+				<li className="list-group-item">Apps Script Projects</li>
+			</Link>
+			<a href="https://www.upwork.com/freelancers/~0184df6f716ff64f05">
+				<li className="list-group-item">Upwork</li>
+			</a>
+			<a href="https://github.com/N8Sayer">
+				<li className="list-group-item">Github</li>
+			</a>
+		</ul>
+	);
 };
 
 export default Nav;
